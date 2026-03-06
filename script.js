163aff898140b20c49855de261ec217cd06cdda0
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-03-06 14:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let r = document.getElementById("radius").value;
+    let radius = parseFloat(r);
+    let volume;
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    if (isNaN(radius) || radius < 0) {
+        volume = NaN;
+    } else {
+        volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        volume = volume.toFixed(4);
+    }
+
+    document.getElementById("volume").value = volume;
+    return false;
+}
+
+window.onload = function () {
+    document.getElementById("MyForm").onsubmit = volume_sphere;
+};
